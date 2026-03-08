1d37546b134e1b741d4a13149f57349b4de751e4
Dekoder-py <kyle@codingcorner.dev>
2026-03-08 21:59:55 +1300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div id="welcome">
+	<h1>Hi, I'm Kyle!</h1>
+	<h2>Welcome to codingcorner.dev!</h2>
+</div>
+<p>I'm a 16 year old developer, hack clubber, and student from New Zealand.</p>
+<p>I like:</p>
+<ul>
+  <li>Coding</li>
+  <li>Neovim</li>
+  <li>Linux</li>
+  <li>Blahaj</li>
+  <li>Ghostty</li>
+  <li><button>88x31 Buttons</button></li> <!-- TODO: Make this open a thing with the buttons -->
+</ul>
